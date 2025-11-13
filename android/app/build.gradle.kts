31f87da92988aecbea504fbc03443509c511e358
Palak8825 <palak.agarwal.8825@gmail.com>
2025-11-14 04:18:18 +0530

    Revert "Temp commit: moved my modules and android changes"
    This reverts commit 8586b64493a3cf01161a1d98d505d91fa7942b50.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,25 +1,14 @@
-import org.jetbrains.kotlin.konan.properties.Properties
-import java.io.FileInputStream
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
-}
-
-// --- Load .env file ---
-val dotenv = Properties().apply {
-    val envFile = rootProject.file(".env")
-    if (envFile.exists()) {
-        load(FileInputStream(envFile))
-    }
 }
 
 android {
-    namespace = "com.example.projects"
+    namespace = "com.example.local_mart"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.14033849"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,20 +16,24 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
-        applicationId = "com.example.projects"
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.local_mart"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
     }
 
     buildTypes {
         release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -48,11 +41,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-firestore")
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,11 +1,3 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-plugins {
-
-    // Google Services plugin for Firebase
-    id("com.google.gms.google-services") version "4.3.15" apply false
-}
-
 allprojects {
     repositories {
         google()
@@ -13,19 +5,17 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory
-    .dir("../../build")
-    .get()
-
-rootProject.layout.buildDirectory.set(newBuildDir)
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.set(newSubprojectBuildDir)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-
